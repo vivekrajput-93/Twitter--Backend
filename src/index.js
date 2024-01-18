4348feee9fde058b90f81f20b7260a094a7078eb
vivekrajput-93 <viveksinghraajputt@gmail.com>
2024-01-18 20:50:44 +0530

    added models to schema

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ const connect = require("./config/database")
 
 const app = express();
 
+const Tweet = require("./models/tweet");
+const TweetRepository = require("./repository/tweet-repo")
+const Comment = require("./models/comment")
+
 
 app.listen(PORT, async() => {
     console.log(`Server Started on ${PORT}`)
     await connect();
     console.log('Mongodb is connected!')
+    // const tweet = await Tweet.create({
+    //     content : "This is my third tweet",
+
+    // })
+    const TweetRepo = new TweetRepository();
+    const tweet =  await TweetRepo.getWithComments('65a940cf5d6d40c2b860be79');
+    console.log(tweet);
 });
+
