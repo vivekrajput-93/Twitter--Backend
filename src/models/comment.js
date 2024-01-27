928764b7f3ae5a43fc581d856cea7a13b13690c8
vivekrajput-93 <viveksinghraajputt@gmail.com>
2024-01-27 14:19:28 +0530

    added toggleLike api

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,9 +6,21 @@ const commentSchema = new mongoose.Schema({
         type : String,
         required : true,
     },
-    userEmail : {
-        type : String,
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : "User"
     },
+    onModel : {
+        type : String,
+        required : true,
+        enum : ["Tweet", "Comment"]
+    },
+    commentable : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        refPath : "onModel",
+    }
 }, {timestamps : true})
 
 const Comment = mongoose.model('Comment', commentSchema);
